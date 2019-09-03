cb52725e5963541765df213f422bca6e27bfa85c
oliveiraDevTech <49378735+oliveiraDevTech@users.noreply.github.com>
2019-09-03 00:10:35 -0300

    inclusão de projeto
    TodoApp

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa História</h2>
+        <p>aisdbisabdasbdasdasdasdnasdn</p>
+        
+    </div>
+)
